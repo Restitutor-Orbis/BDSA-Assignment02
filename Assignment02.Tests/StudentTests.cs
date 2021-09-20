ad4648c058bd4513185afff8d810f72bc79e3125
vson <vson@itu.dk>
2021-09-20 10:47:15 +0200

    Add further test cases

--- Assignment02.Tests/StudentTests.cs
+++ Assignment02.Tests/StudentTests.cs
@@ -11,7 +11,15 @@ namespace Library.Tests
             //start date, end date, graduation date, expected status
             new object[] {new DateTime(2020, 2, 20), new DateTime(2023, 2, 20), new DateTime(2023, 2, 20), Status.Active},
             new object[] {new DateTime(2020, 2, 20), new DateTime(2021, 2, 20), new DateTime(2023, 2, 20), Status.Dropout},
-            new object[] {new DateTime(2010, 1, 1), new DateTime(2015, 6, 29), new DateTime(2015, 6, 29), Status.Graduated}
+            new object[] {new DateTime(2000, 1, 1), new DateTime(2000, 12, 30), new DateTime(3000, 7, 1), Status.Dropout},
+            new object[] {new DateTime(2010, 1, 1), new DateTime(2015, 6, 29), new DateTime(2015, 6, 29), Status.Graduated},
+            new object[] {new DateTime(1900, 1, 1), new DateTime(2021, 7, 1), new DateTime(2015, 7, 1), Status.Graduated},
+            new object[] {new DateTime(2022, 9, 1), new DateTime(2025, 7, 1), new DateTime(2025, 7, 1), Status.New},
+            new object[] {new DateTime(2100, 9, 1), new DateTime(2200, 7, 1), new DateTime(2200, 7, 1), Status.New},
+            
+            new object[] {new DateTime(2020, 1, 1), new DateTime(2010, 1, 1), new DateTime(2000, 7, 1), Status.Invalid},
+            new object[] {new DateTime(2020, 1, 1), new DateTime(2010, 1, 1), new DateTime(2023, 7, 1), Status.Invalid},
+            new object[] {new DateTime(2120, 1, 1), new DateTime(2023, 1, 1), new DateTime(2023, 1, 1), Status.Invalid},
         };
 
 
