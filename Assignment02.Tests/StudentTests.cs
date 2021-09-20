c8cf1e65cd4a6c0a8c316ebcba17167c6614ebb5
Restitutor-Orbis <65425000+Restitutor-Orbis@users.noreply.github.com>
2021-09-20 11:19:15 +0200

    Clean code (#5)
    * Implement initial tests
    
    * Add further test cases
    
    * Add error prevention
    
    * Add error prevention
    
    * Clean code
    
    Co-authored-by: Crushaderdk <46741410+Crushaderdk@users.noreply.github.com>

--- Assignment02.Tests/StudentTests.cs
+++ Assignment02.Tests/StudentTests.cs
@@ -11,40 +11,23 @@ namespace Library.Tests
             //start date, end date, graduation date, expected status
             new object[] {new DateTime(2020, 2, 20), new DateTime(2023, 2, 20), new DateTime(2023, 2, 20), Status.Active},
             new object[] {new DateTime(2020, 2, 20), new DateTime(2021, 2, 20), new DateTime(2023, 2, 20), Status.Dropout},
-            new object[] {new DateTime(2000, 1, 1), new DateTime(2000, 12, 30), new DateTime(3000, 7, 1), Status.Dropout},
-            new object[] {new DateTime(2010, 1, 1), new DateTime(2015, 6, 29), new DateTime(2015, 6, 29), Status.Graduated},
-            new object[] {new DateTime(1900, 1, 1), new DateTime(2021, 7, 1), new DateTime(2015, 7, 1), Status.Graduated},
-            new object[] {new DateTime(2022, 9, 1), new DateTime(2025, 7, 1), new DateTime(2025, 7, 1), Status.New},
-            new object[] {new DateTime(2100, 9, 1), new DateTime(2200, 7, 1), new DateTime(2200, 7, 1), Status.New},
+
+            new object[] {new DateTime(2000, 1, 1),  new DateTime(2000, 12, 30), new DateTime(3000, 7, 1), Status.Dropout},
+            new object[] {new DateTime(2010, 1, 1),  new DateTime(2015, 6, 29), new DateTime(2015, 6, 29), Status.Graduated},
+            new object[] {new DateTime(1900, 1, 1),  new DateTime(2021, 7, 1), new DateTime(2015, 7, 1), Status.Graduated},
+            new object[] {new DateTime(2022, 9, 1),  new DateTime(2025, 7, 1), new DateTime(2025, 7, 1), Status.New},
+            new object[] {new DateTime(2100, 9, 1),  new DateTime(2200, 7, 1), new DateTime(2200, 7, 1), Status.New},
             
-            new object[] {new DateTime(2020, 1, 1), new DateTime(2010, 1, 1), new DateTime(2000, 7, 1), Status.Invalid},
-            new object[] {new DateTime(2020, 1, 1), new DateTime(2010, 1, 1), new DateTime(2023, 7, 1), Status.Invalid},
-            new object[] {new DateTime(2120, 1, 1), new DateTime(2023, 1, 1), new DateTime(2023, 1, 1), Status.Invalid},
+            new object[] {new DateTime(2020, 1, 1),  new DateTime(2010, 1, 1), new DateTime(2000, 7, 1), Status.Invalid},
+            new object[] {new DateTime(2020, 1, 1),  new DateTime(2010, 1, 1), new DateTime(2023, 7, 1), Status.Invalid},
+            new object[] {new DateTime(2120, 1, 1),  new DateTime(2023, 1, 1), new DateTime(2023, 1, 1), Status.Invalid},
         };
 
 
-        [Fact]
-        public void student_readStatus_returnTrue()
-        {
-            //Arrange
-            DateTime start = new DateTime(2020, 2, 20);
-            DateTime end = new DateTime(2023, 2, 20);
+           
 
-            Student s = new Student();
 
-            //Act & Assert
-            try
-            {
-               Status st = s.Status;  
-            }
-            catch (System.Exception)
-            {
-                //if can't read student status, test fails
-                Assert.True(false);
-            }
-
-            Assert.True(true);
-        }
+    
 
         [Theory, MemberData(nameof(StudentDatesData))]
         public void student_status_correctlyAssigned(DateTime startDate, DateTime endDate, DateTime graduationDate, Status expected) 
@@ -60,30 +43,6 @@ namespace Library.Tests
 
             //Assert
             Assert.Equal(expected, actual);
-        }
-
-        /* Compiler doesn't allow this, so test is redundant
-
-        [Fact]
-        public void student_setStatus_catchException() {
-            //Arrange
-            DateTime start = new DateTime(2020, 2, 20);
-            DateTime end = new DateTime(2023, 2, 20);
-
-            Student s = new Student();
-            Status st = Status.Graduated;
-
-            //Act & Assert
-            try
-            {
-               s.Status = st;
-               Assert.True(false); //if can set read student status, test fails
-            }
-            catch (System.Exception)
-            {
-                Assert.True(true);
-            }
-        }
-        */
+        }      
     }
 }
